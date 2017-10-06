87f48f06a9b4fe95a7b3e47847e1f086bf01c2fb
baztian <bastian.dev@gmail.com>
2017-10-06 11:18:23 +0200

    Find out about config

--- push.sh
+++ push.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 #git config --global user.email "travis@travis-ci.org"
 #git config --global user.name "Travis CI"
+export
+git config --list
 git push origin HEAD:${TRAVIS_BRANCH}
